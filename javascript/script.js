dcc8c84f64f950164ca72da497e562207384b58f
Manal Alghamdi <manalalghamdi0@gmail.com>
2021-03-13 14:11:27 +0300

    add 3 divs

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,22 @@
+
 $(document).ready(function(){
+    
     AOS.init();
-    function handleFirstPlay(event) {
-        let vid = event.target;
+
+ 
+   
+    new Splide( '.splide', {
+      type     : 'loop',
+      autoWidth: true,
+      focus    : 'center',
+      number : 300 ,
+      autoplay : true,
+      interval : 3000,
+      direction  : 'ttb',
+      heightRatio: 0.6,
+	    perPage    : 3,
+      gap : 2,
+    } ).mount();
+
       
-        vid.onplay = null;
-      
-        // Start whatever you need to do after playback has started
-      }
 });
